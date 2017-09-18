12ca8a4aeeaa91c9999c007972c23ad62cb2dc10
Wintun <wintun@neuro10.com>
2017-09-18 18:28:01 +0800

    added api annotations to describe API info

--- backend/src/main/java/com/hlaing/ng4boot/controller/WebRestController.java
+++ backend/src/main/java/com/hlaing/ng4boot/controller/WebRestController.java
@@ -1,12 +1,18 @@
 package com.hlaing.ng4boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value="ng4boot", description="Spring Boot Rest api")
 public class WebRestController {
 	
-	@RequestMapping("/api/hello")
+	@ApiOperation(value="Get the hello api")
+	@RequestMapping(value="/api/hello", method=RequestMethod.GET)
 	public String hello() {
 		return "Hello Angular4 SpringBoot App";
 	}
